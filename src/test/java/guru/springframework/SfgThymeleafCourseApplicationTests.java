f7a72c44b60fc630dd96170d5462b4fa5f85a83c
vinay61 <37111184+vinay61@users.noreply.github.com>
2019-05-09 22:38:20 +0530

    added comment line

--- src/test/java/guru/springframework/SfgThymeleafCourseApplicationTests.java
+++ src/test/java/guru/springframework/SfgThymeleafCourseApplicationTests.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @WebAppConfiguration
 public class SfgThymeleafCourseApplicationTests {
 
+	/** comment */
 	@Test
 	public void contextLoads() {
 	}
